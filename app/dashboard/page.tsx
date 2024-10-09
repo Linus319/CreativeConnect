9597efd6e23b692ab8c32a40ac764cd45c5cd94a
TechNinja14 <abel.ochoa01@utrgv.edu>
2024-10-09 15:39:43 -0500

    added some mock pages

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+	return (
+		<div className="flex h-screen">
+		<div className="m-auto">
+		<h1>Dashboard goes here</h1>
+		</div>
+		</div>
+
+	);
+}
---
+++ app/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function Signup() {
+	return (
+		<div className="flex h-screen">
+		<div className="m-auto">
+		<h1>This is the Signup Page</h1>
+		</div>
+		</div>
+
+	);
+}
