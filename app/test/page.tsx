e1b258e560aa2b26924b72e2d66f97ab41950304
TechNinja14 <abel.ochoa01@utrgv.edu>
2024-10-19 13:59:09 -0500

    added tailwind, etc

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -2,7 +2,23 @@ import { createClient } from '@/utils/supabase/server';
 
 export default async function Test() {
 	const supabase = createClient();
-	const { data: users } = await supabase.from("Test Table").select();
+	const { data: users } = await supabase.from("users").select();
 
-	return <pre>{JSON.stringify(users, null, 2)}</pre>
+	const { data: images } = await supabase.from("images").select();
+
+	const { data: audio } = await supabase.from("audio").select();
+
+	const { data: video } = await supabase.from("video").select();
+
+	const image_list = images.map(img => <div><img src={img.url}/></div>);
+	return (
+		<>
+			<div className="flex flex-row">
+			{image_list}
+			</div>
+		</>
+	);
+
+
+	//return <pre>{JSON.stringify(video, null, 2)}</pre>
 }
