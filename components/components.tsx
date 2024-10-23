8a21d3fba048a30657799b6754c478a32dc9c00c
Abel Ochoa <abel.ochoa01@utrgv.edu>
2024-10-23 18:40:06 -0500

    fixed images being null

--- components/components.tsx
+++ components/components.tsx
@@ -5,7 +5,7 @@ export async function Images() {
 
   const { data: images } = await supabase.from("images").select();
 
-	const image_list = images.map(img => <div className="flex-auto"><a href={img.url}><img src={img.url} className="rounded-lg"/></a></div>);
+	const image_list = images?.map(img => <div className="flex-auto"><a href={img.url}><img src={img.url} className="rounded-lg"/></a></div>);
 
 	return (
 		<div className="grid grid-cols-4 m-5 gap-4">
