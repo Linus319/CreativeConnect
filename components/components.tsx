398f028a40d5cff88ee448cb7ab44835610793a6
Gabe-GH <gabriel.lira01@utrgv.edu>
2024-10-23 19:50:34 -0500

    fixed conflicts

--- components/components.tsx
+++ components/components.tsx
@@ -5,11 +5,7 @@ export async function Images() {
 
   const { data: images } = await supabase.from("images").select();
 
-<<<<<<< HEAD
 	const image_list = images?.map(img => <div className="flex-auto"><a href={img.url}><img src={img.url} className="rounded-lg"/></a></div>);
-=======
-  const image_list = images?.map(img => <div className="flex-auto"><a href={img.url}><img src={img.url} className="rounded-lg" /></a></div>);
->>>>>>> ac550db56788fe34e174526473c413664c423669
 
   return (
     <div className="grid grid-cols-4 m-5 gap-4">
