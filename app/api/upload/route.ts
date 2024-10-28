4c9604b04b203bcaa43139303980584b11a226c8
TechNinja14 <abel.ochoa01@utrgv.edu>
2024-10-28 15:20:32 -0500

    removed extra code

--- app/api/upload/route.ts
+++
@@ -1,7 +0,0 @@
-export async function POST(request: Request): Promise<Response> {
-  const formData = await request.formData();
-  
-  console.log(formData.getAll('files'));
-  console.log("post successfull");
-  return {};
-}
