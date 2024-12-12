c04e0f5f50d84d259ba9c8838404df9802890fae
Thomas <thomas.fagan01@utrgv.edu>
2024-12-11 18:05:35 -0600

    browse ui and profile image on menu

--- components/header-auth-client.tsx
+++ components/header-auth-client.tsx
@@ -8,26 +8,39 @@ import { signOutAction } from '../app/actions';
 export default function UserMenu() {
 
   const [showMenu, setShowMenu] = useState(false);
+  const [image, setImage] = useState<string | null>(null);
+
+  useEffect(() => {
+    fetch('/api/get-profile-pic', { body: new FormData(), method: 'POST'})
+    .then((res) => res.json())
+    .then((data) => { 
+      setImage(data);
+    });
+  }, []);
 
   function toggleMenu() {
     setShowMenu(!showMenu);
   }
-// (make server action? so supabase isn't summoned on client component here)
-  return (<div className="relative" onMouseEnter={toggleMenu} onMouseLeave={toggleMenu}>
-            <img className="size-10" src="https://www.seekpng.com/png/detail/365-3651600_default-portrait-image-generic-profile.png" />
-            {showMenu ? 
-              <div className="flex flex-col absolute -bottom-30 right-0 bg-gray-500 rounded-md divide-y divide-gray-300">
-                <Link href="/dashboard" className="p-2">Dashboard</Link>
-                <Link href="/profile" className="p-2">Profile</Link>
-                <Link href="/profile/edit" className="p-2">Settings</Link>
-                <form action={signOutAction} className="p-2">
-                  <button type="submit">
-                    Sign out
-                  </button>
-                </form>
-              </div>
-              :
-              <></>}
-          </div>);
+
+  return (
+    <div className="relative" onMouseEnter={toggleMenu} onMouseLeave={toggleMenu}>
+      <img 
+        className="rounded-full size-10" 
+        src={image ? image : "https://www.seekpng.com/png/detail/365-3651600_default-portrait-image-generic-profile.png" }
+      />
+      {showMenu ? 
+        <div className="flex flex-col absolute -bottom-30 right-0 bg-gray-500 rounded-md divide-y divide-gray-300">
+          <Link href="/dashboard" className="p-2">Dashboard</Link>
+          <Link href="/profile" className="p-2">Profile</Link>
+          <Link href="/profile/edit" className="p-2">Settings</Link>
+          <form action={signOutAction} className="p-2">
+            <button type="submit">
+              Sign out
+            </button>
+          </form>
+        </div>
+        :
+        <></>}
+    </div>);
 
 }
